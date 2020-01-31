d6ebd0d602d13db4b0cfffae341a7b28c2ad677f
rich oates <oates1@gmail.com>
2020-01-31 22:59:26 +0100

    use binding for nbrUnits on hex

--- de.rjo.hex.game/src/main/java/de/rjo/game/GameState.java
+++ de.rjo.hex.game/src/main/java/de/rjo/game/GameState.java
@@ -1,34 +1,42 @@
 package de.rjo.game;
 
 import de.rjo.hex.Hexagon;
+import javafx.beans.binding.StringBinding;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.scene.control.Label;
-import javafx.scene.text.Font;
 
 // stores the game state for a particular hexagon
 public class GameState {
 
     private Team team = Team.NOT_SET;
-    private int nbrUnits = 0;
-    private Label label; // this display the info
+    private IntegerProperty nbrUnits = new SimpleIntegerProperty(0);
+    private Label label; // this display the info in the hexagon itself
 
     public GameState() {
 	label = new Label("");
-	label.setFont(new Font(20));
-//	label.setVisible(false);
+	label.getStyleClass().add("gamestate");
+	// need a special binding, since nbrUnits should not be displayed if zero
+	StringBinding sb = new StringBinding() {
+	    {
+		super.bind(nbrUnits);
+	    }
+
+	    @Override
+	    protected String computeValue() {
+		return (nbrUnits.get() > 0) ? "" + nbrUnits.get() : "";
+	    }
+	};
+	label.textProperty().bind(sb);
     }
 
     public boolean isEmpty() {
-	return nbrUnits == 0;
+	return nbrUnits.get() == 0;
     }
 
     public void setUnits(Team team, int nbrUnits) {
 	this.team = team;
-	this.nbrUnits = nbrUnits;
-	updateLabel();
-    }
-
-    private void updateLabel() {
-	this.label.setText((nbrUnits > 0) ? "" + nbrUnits : "");
+	this.nbrUnits.set(nbrUnits);
     }
 
     public Team getTeam() {
@@ -36,7 +44,7 @@ public class GameState {
     }
 
     public int getNbrUnits() {
-	return nbrUnits;
+	return nbrUnits.get();
     }
 
     public Label getLabel() {
@@ -44,17 +52,15 @@ public class GameState {
     }
 
     public void increment(int nbr) {
-	if (nbrUnits != 0) {
-	    nbrUnits += nbr;
-	    updateLabel();
+	if (nbrUnits.get() != 0) {
+	    nbrUnits.set(nbrUnits.get() + nbr);
 	}
     }
 
     public void decrement(final Hexagon hex, int nbr) {
-	if (nbrUnits >= nbr) {
-	    nbrUnits -= nbr;
-	    updateLabel();
-	    if (nbrUnits == 0) {
+	if (nbrUnits.get() >= nbr) {
+	    nbrUnits.set(nbrUnits.get() - nbr);
+	    if (nbrUnits.get() == 0) {
 		team = Team.NOT_SET;
 		hex.setStyleClass(Team.NOT_SET.getStyleClass());
 	    }
@@ -65,7 +71,7 @@ public class GameState {
      * can the given number of units be removed from this hex?
      */
     public boolean canMoveFrom(int nbr) {
-	return nbrUnits >= nbr;
+	return nbrUnits.get() >= nbr;
     }
 
     /**
